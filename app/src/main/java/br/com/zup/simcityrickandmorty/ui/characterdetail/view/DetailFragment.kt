004b55f636a159262cf0ff1c7d397751bd7218a7
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-07-09 01:09:23 -0300

    Implementar detail view

--- app/src/main/java/br/com/zup/simcityrickandmorty/ui/characterdetail/view/DetailFragment.kt
+++ app/src/main/java/br/com/zup/simcityrickandmorty/ui/characterdetail/view/DetailFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import br.com.zup.simcityrickandmorty.R
+import br.com.zup.simcityrickandmorty.const.CHARACTER
+import br.com.zup.simcityrickandmorty.data.model.CharactersResult
 import br.com.zup.simcityrickandmorty.databinding.FragmentDetailBinding
+import br.com.zup.simcityrickandmorty.ui.home.view.HomeActivity
+import com.squareup.picasso.Picasso
 
 class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
@@ -15,5 +21,27 @@ class DetailFragment : Fragment() {
     ): View {
         binding = FragmentDetailBinding.inflate(inflater,container,false)
         return binding.root
+    }
+    private fun dataRecover(){
+        val character = arguments?.getParcelable<CharactersResult>(CHARACTER)
+        character?.let {
+            Picasso.get().load(character.image).into(binding.ivDetailImage)
+            binding.tvDetailCharacterName.text = it.name
+            binding.tvDetailCharacterStatus.text = it.status
+            binding.tvDetailCharacterGender.text = it.gender
+            binding.tvDetailCharacterSpecie.text = it.species
+
+            binding.ivIcon.setImageDrawable(
+                ContextCompat.getDrawable(binding.root.context,
+                if(it.isFavorite) R.drawable.ic_favorite
+                else R.drawable.ic_disfavor)
+            )
+            (activity as HomeActivity).supportActionBar?.title = it.name
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataRecover()
     }
 }
--- app/src/main/java/br/com/zup/simcityrickandmorty/ui/characterdetail/viewmodel/DetailViewModel.kt
+++ app/src/main/java/br/com/zup/simcityrickandmorty/ui/characterdetail/viewmodel/DetailViewModel.kt
@@ -2,17 +2,27 @@ package br.com.zup.simcityrickandmorty.ui.characterdetail.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import br.com.zup.simcityrickandmorty.const.ERROR
 import br.com.zup.simcityrickandmorty.data.model.CharactersResult
 import br.com.zup.simcityrickandmorty.domain.model.SingleLiveEvent
 import br.com.zup.simcityrickandmorty.domain.usecase.CharacterUseCase
 import br.com.zup.simcityrickandmorty.ui.viewstate.ViewState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class DetailViewModel(application: Application): AndroidViewModel(application) {
     private val useCase = CharacterUseCase(application)
-    val characterState = SingleLiveEvent<ViewState<List<CharactersResult>>>()
+    val characterState = MutableLiveData<ViewState<List<CharactersResult>>>()
 
     suspend fun characterDetail(){
-
+        try{
+            val response = withContext(Dispatchers.IO){
+                useCase.getCharactersAPI()
+            }
+            characterState.value = response
+        }catch(e:Exception){
+            characterState.value = ViewState.Error(Throwable(ERROR))
+        }
     }
-
 }
