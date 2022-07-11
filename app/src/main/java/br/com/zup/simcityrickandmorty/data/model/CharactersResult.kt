4bb264a16485087a040278d7073f8a81d1e3b0b9
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-07-11 13:42:38 -0300

    Organizar os atributos da entity

--- app/src/main/java/br/com/zup/simcityrickandmorty/data/model/CharactersResult.kt
+++ app/src/main/java/br/com/zup/simcityrickandmorty/data/model/CharactersResult.kt
@@ -12,20 +12,28 @@ data class CharactersResult(
     @PrimaryKey(autoGenerate = false)
     @SerializedName("id")
     val id: Int = 0,
+
     @SerializedName("created")
     val created: String = "",
+
     @SerializedName("gender")
     val gender: String = "",
+
     @SerializedName("image")
     val image: String = "",
+
     @SerializedName("name")
     val name: String = "",
+
     @SerializedName("species")
     val species: String = "",
+
     @SerializedName("status")
     val status: String = "",
+
     @SerializedName("type")
     val type: String = "",
+
     @SerializedName("url")
     val url: String = "",
 
